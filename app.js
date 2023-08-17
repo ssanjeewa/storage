7e0f463498a4611265754822843590b5409ddca9
Sanjeewa Silva <ssanjeewa@gmail.com>
2023-08-17 19:33:34 +0530

    add cors

--- app.js
+++ app.js
@@ -24,7 +24,9 @@ app.get("/", (req, res) => {
 });
 
 app.get('/download/:file', (req, res) => {
+    console.log(__dirname +"/files/" + req.params.file)
     res.download(__dirname +"/files/" + req.params.file, (err) => {
+        
         if (err) {
           res.status(500).send({
             message: "Could not download the file. " + err,
